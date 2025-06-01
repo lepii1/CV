f05cba68abc1e064ef33b64fe7b9ae3c550025e5
Ahmad Aulia Fahlevi <aauliafahlefi@gmail.com>
2025-06-01 18:54:29 +0700

    initial commit

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -25,8 +25,8 @@ document.addEventListener("DOMContentLoaded", function () {
   const iconImg = document.getElementById("theme-icon");
   const body = document.body;
 
-  const sunIcon = "{{site.baseurl}}/assets/images/hidup.png";
-  const moonIcon = "/assets/images/mati.png";
+  const sunIcon = "/CV/assets/images/hidup.png";
+  const moonIcon = "/CV/assets/images/mati.png";
 
   // Cek preferensi
   if (localStorage.getItem("theme") === "dark") {
